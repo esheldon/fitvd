ec6ddd2dfff374674f81f80c01818e3bf9daf1c1
Erin Sheldon <erin.sheldon@gmail.com>
2019-06-13 06:51:36 -0400

    deal with empty mask

--- fitvd/masks.py
+++ fitvd/masks.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
-
 import numpy as np
+
 
 class Mask(object):
     def __init__(self, fname, config):
-        self.fname=fname
+        self.fname = fname
         self._set_config(config)
         self._load_mask()
 
@@ -13,21 +13,27 @@ class Mask(object):
         rad_mult = self.config['rad_mult']
 
         self.maskdata = read_mask(self.fname)
-        self.scat = smatch.Catalog(
-            self.maskdata['ra'],
-            self.maskdata['dec'],
-            rad_mult*self.maskdata['rad_arcsec']/3600.0,
-        )
+        if self.maskdata.size > 0:
+            self.scat = smatch.Catalog(
+                self.maskdata['ra'],
+                self.maskdata['dec'],
+                rad_mult*self.maskdata['rad_arcsec']/3600.0,
+            )
+        else:
+            self.scat = None
 
     def is_masked(self, ra, dec):
         """
         check if the input positions are masked
         """
+        is_masked = np.zeros(ra.size, dtype='bool')
+
+        if self.maskdata.size == 0:
+            return is_masked
 
         matches = self._match(ra, dec)
 
-        is_masked = np.zeros(ra.size, dtype='bool')
-        is_masked[ matches['i2'] ] = True
+        is_masked[matches['i2']] = True
         return is_masked
 
     def is_unmasked(self, ra, dec):
@@ -53,33 +59,32 @@ class Mask(object):
         if config is not None:
             self.config.update(config)
 
+
 def read_mask(fname):
     """
     load a mask file
 
     Parameters
     ----------
-    tilename: string
-        Either the basic tilename such as SN-C3_C10
-        or with reqnum/attnum SN-C3_C10_r3688p01
+    fname: string
+        path to the file
     """
     print('loading mask from: %s' % fname)
 
-    data={}
+    data = {}
     with open(fname) as fobj:
         for line in fobj:
             if '#' in line or '---' in line:
                 continue
 
             ls = line.split()
-            ind = int( ls[0] )
-            ra = float( ls[1] )
-            dec = float( ls[2] )
-            band = ls[3]
+            ind = int(ls[0])
+            ra = float(ls[1])
+            dec = float(ls[2])
             rad_arcsec = float(ls[4])
-            
+
             if ind in data:
-                radmax = max( rad_arcsec, data[ind]['rad_arcsec'])
+                radmax = max(rad_arcsec, data[ind]['rad_arcsec'])
                 data[ind]['rad_arcsec'] = radmax
             else:
                 data[ind] = {
@@ -89,23 +94,20 @@ def read_mask(fname):
                     'rad_arcsec': rad_arcsec,
                 }
 
-
     dt = [
-        ('id','i8'),
-        ('ra','f8'),
-        ('dec','f8'),
-        ('rad_arcsec','f8'),
+        ('id', 'i8'),
+        ('ra', 'f8'),
+        ('dec', 'f8'),
+        ('rad_arcsec', 'f8'),
     ]
 
     st = np.zeros(len(data), dtype=dt)
 
-    for i,key in enumerate(data):
-        d = data[key] 
-        st['id'][i] = d['id']  
-        st['ra'][i] = d['ra']  
-        st['dec'][i] = d['dec']  
-        st['rad_arcsec'][i] = d['rad_arcsec']  
+    for i, key in enumerate(data):
+        d = data[key]
+        st['id'][i] = d['id']
+        st['ra'][i] = d['ra']
+        st['dec'][i] = d['dec']
+        st['rad_arcsec'][i] = d['rad_arcsec']
 
     return st
-
-
