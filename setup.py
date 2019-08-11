a8bab60d47e4fb61f783168666bc79a6bc430a5a
Erin Sheldon <erin.sheldon@gmail.com>
2019-08-11 10:43:32 -0400

    bumping vers to v0.9.5

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ scripts = [s for s in scripts if '~' not in s]
 
 
 setup(
-    name="fitvd", 
-    version="v0.9.4",
+    name="fitvd",
+    version="v0.9.5",
     description="Code to fit models to objects in DES+VISTA using MOF",
     license = "GPL",
     author="Erin Scott Sheldon",
